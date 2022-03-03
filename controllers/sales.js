740fe4affff735a7b6cc99a35127e22f5eaacbe4
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-03-03 16:22:05 -0300

    Add refactoring of sales controllers file functions

--- controllers/sales.js
+++ controllers/sales.js
@@ -2,9 +2,16 @@ const Sales = require('../services/sales');
 
 const getAll = async (_req, res, next) => {
   try {
-    const sales = await Sales.getAll('author');
+    const sales = await Sales.getAll();
+
+    if (!sales) {
+      const error = { status: 500, message: 'Internal server error' };
+      throw error; 
+    }
+
     return res.status(200).json(sales);
   } catch (error) {
+    console.log(error);
     next(error);
   }
 };
@@ -21,6 +28,7 @@ const findById = async (req, res, next) => {
 
     return res.status(200).json(sale);
   } catch (error) {
+    console.log(error);
     next(error);
   }
 };
@@ -36,30 +44,42 @@ const create = async (req, res, next) => {
 
   return res.status(201).json(sale);
   } catch (error) {
+    console.log(error);
     next(error);
   }
 };
 
-const update = async (req, res) => {
-  const { id } = req.params;
+const update = async (req, res, next) => {
+  try {
+    const { id } = req.params;
   const sales = req.body;
   const salesResult = await Sales.update({ sales, id });
+
+  if (!salesResult) {
+    const err = { status: 404, message: 'Sale not found' };
+      throw err;
+  }
+
   return res.status(200).json(salesResult);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 };
 
 const destroy = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const product = await Sales.destroy(id);
+    const salesResult = await Sales.destroy(id);
   
-    if (!product) {
+    if (!salesResult) {
       const err = { status: 404, message: 'Sale not found' };
         throw err;
     }
   
     return res.status(204).end();
   } catch (error) {
-    console.log('catch-log:', error);
+    console.log(error);
     next(error);
   }
 };
